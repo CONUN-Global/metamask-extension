bbdfc3a85f88c27e7b586b88b2c32fdbd6609b70
BruceJi7 <revolverc24@gmail.com>
2022-02-11 17:15:05 +0900

    Code structure improvements

--- metacon/config/index.js
+++ metacon/config/index.js
@@ -1,7 +1,7 @@
 export const metacon = {
   testnet: {
     web3Url: 'https://ropsten.infura.io/v3/2b1758a74cf249a598f13e357bb058dc',
-    serverUrl: 'http://192.168.100.172:80/api/v1/',
+    serverUrl: 'http://192.168.100.202:80/api/v1/',
   },
   org: 'Org1',
   walletType: 'ETH',
